281625934650decdf520d85211ab6c407083fdd5
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-04-16 12:54:10 -0700

    updated gitignore

--- AppSync.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "graphqlEndpoint": "https://26fhtrk6kjfxvlfbvj4ijdlgvi.appsync-api.us-east-1.amazonaws.com/graphql",
-  "region": "us-east-1",
-  "authenticationType": "API_KEY",
-  "apiKey": "da2-wi7ra6blync4zgpyphbmaxoeea"
-}
